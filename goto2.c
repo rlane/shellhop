f5691f94511332d76b5b0a1415122451f66f628e
Rich Lane <lanerl@gmail.com>
2017-04-03 23:58:06 -0700

    search for needle

--- goto2.c
+++ goto2.c
@@ -15,25 +15,42 @@ int main(int argc, char** argv) {
   fprintf(stderr, "\e[?25l");  // Hide cursor.
   fprintf(stderr, "goto2: %s", line);
   fflush(stderr);
-  char c1 = getchar();
 
-  fprintf(stderr, "\e[G");  // Move to column 0.
-  fprintf(stderr, "\e[2K");  // Clear line.
-  fprintf(stderr, "goto2: ");
-  for (int i = 0, k = 'a'; line[i]; i++) {
-    if (line[i] == c1 && k <= 'z') {
-      fprintf(stderr, "\e[7m%c\e[0m", k++);  // Reverse video.
-    } else {
-      fputc(line[i], stderr);
+  char needle[64] = "";
+  int needle_len = 0;
+
+  while (1) {
+    fprintf(stderr, "\e[G");  // Move to column 0.
+    fprintf(stderr, "\e[2K");  // Clear line.
+    fprintf(stderr, "goto2: ");
+    int remain = 0;
+    for (int i = 0; line[i]; i++) {
+      if (needle_len > 0 && !strncmp(line + i, needle, needle_len)) {
+        fprintf(stderr, "\e[4;7m%c\e[0m", line[i]);  // Reverse video, underline.
+        remain = needle_len - 1;
+      } else if (remain > 0) {
+        fprintf(stderr, "\e[7m%c\e[0m", line[i]);  // Reverse video.
+        remain--;
+      } else {
+        fputc(line[i], stderr);
+      }
     }
-  }
-  fflush(stderr);
-
-  char c2 = getchar();
-  for (int i = 0, k = 'a'; line[i] && k <= 'z'; i++) {
-    if (line[i] == c1 && k++ == c2) {
-      printf("%d\n", i);
+    fflush(stderr);
+    char c = getchar();
+    if (c == '\r') {
+      char* p = strstr(line, needle);
+      if (p) {
+        printf("%d\n", (int)(p - line));
+      }
       break;
+    } else if (c == 127 /* DEL */) {
+      if (needle_len > 0) {
+        needle[--needle_len] = '\0';
+      }
+    } else if (c == '\e') {
+      break;
+    } else if (needle_len + 1< sizeof(needle)) {
+      needle[needle_len++] = c;
     }
   }
 
