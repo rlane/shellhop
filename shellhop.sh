2a5cfc6325fd7092f788ba705b6396bbf6658af2
Rich Lane <lanerl@gmail.com>
2017-04-08 16:19:26 -0700

    use beginning-of-line readline function

--- shellhop.sh
+++ shellhop.sh
@@ -2,6 +2,6 @@ function _shellhop {
   READLINE_POINT=$(shellhop "$READLINE_LINE")
 }
 
-bind '"\key1":"\C-a"'
+bind '"\key1":beginning-of-line'
 bind -x '"\key2":"_shellhop"'
 bind '"\C-k":"\key1\key2"'
