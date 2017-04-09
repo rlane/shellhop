4c87de3fbcb646c6bdd1656299afd40460ba2558
Rich Lane <lanerl@gmail.com>
2017-04-08 19:32:20 -0700

    improve keybindings

--- shellhop.sh
+++ shellhop.sh
@@ -2,6 +2,6 @@ function _shellhop {
   READLINE_POINT=$(shellhop "$READLINE_LINE")
 }
 
-bind '"\key1":beginning-of-line'
-bind -x '"\key2":"_shellhop"'
-bind '"\C-k":"\key1\key2"'
+bind '"\C-xS0":beginning-of-line'
+bind -x '"\C-xS1":"_shellhop"'
+bind '"\C-x\C-f":"\C-xS0\C-xS1"'
